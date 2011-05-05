0c5f10026e9af53f5c8efa2ab622ec231285ef38
Bearstech rse michka <rse@michka.bearstech.com>
2011-05-05 12:11:15 +0200

    fix middleware

--- authtkt/middleware.py
+++ authtkt/middleware.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils import simplejson
 from authtkt.auth_tkt import AuthTktCookiePlugin
+import logging
+
+log = logging.getLogger(__name__)
 
 def resolve(module):
     if module:
@@ -27,7 +30,7 @@ class AuthTktMiddleware(object):
             try:
                 user = User.objects.get(id=user_id)
             except User.DoesNotExist:
-                user = User(id=id)
+                user = User(id=user_id)
                 self.callback(user)
                 user.save()
             user.backend='django.contrib.auth.backends.ModelBackend'
