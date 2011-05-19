1d26b55f0bc9af1a76476019d5448561b3fd47ff
Gael Pasgrimaud <gael@gawel.org>
2011-05-19 09:47:40 +0200

    dependencie

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2'
+version = '0.3'
 
 setup(name='django-authtkt',
       version=version,
@@ -21,6 +21,7 @@ setup(name='django-authtkt',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'Paste',
       ],
       entry_points="""
       # -*- Entry points: -*-
